31e92ac6cfced92a659c207af1434d7baa45d4ae
Victor_Ma <389796272@qq.com>
2020-06-13 23:51:30 -0400

    World Shifting

---
+++ SummerStudio/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int z1, z2;
+
+    void Start()
+    {
+        z1 = -10;
+        z2 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(this.transform.position.z < z2)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, z2);
+            }
+            else if(this.transform.position.z > z1)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, z1);
+            }
+        }
+    }
+}
---
+++ SummerStudio/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    Camera maincam;
+    int z1, z2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        z1 = -10;
+        z2 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 2);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 2);
+        }
+
+    }
+}
