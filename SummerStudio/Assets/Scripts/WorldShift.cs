43eabf94305f6a46129a8db142a74c24a62e5e62
SckrPunch <40640248+SckrPunch@users.noreply.github.com>
2020-06-26 06:27:11 -0400

    Hiding opposite world
    When in physical world, kenos world is hidden.
    
    When in kenos world, physical world is hidden.

--- SummerStudio/Assets/Scripts/WorldShift.cs
+++ SummerStudio/Assets/Scripts/WorldShift.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class WorldShift : MonoBehaviour
 {
-    public bool w_Type = true; //true = physical world; false = other world
-    private bool ignore_Layer = true; 
+    public bool w_Type = true; //true = physical world; false = kenos world
+    private bool ignore_Layer = true;
+    public GameObject physical_world;
+    public GameObject kenos_world;
 
     // Start is called before the first frame update
     void Start()
     {
+        ToggleWorldVisibility(w_Type);
         Physics2D.IgnoreLayerCollision(8, 11, ignore_Layer);
     }
 
@@ -18,10 +21,26 @@ public class WorldShift : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            w_Type = !w_Type;
-            Physics2D.IgnoreLayerCollision(8, 10, ignore_Layer);
-            Physics2D.IgnoreLayerCollision(8, 11, !ignore_Layer);
-            ignore_Layer = !ignore_Layer;
+            w_Type = !w_Type; //toggle world type on each button press
+            Physics2D.IgnoreLayerCollision(8, 10, ignore_Layer); //toggle ignore on physical layer
+            Physics2D.IgnoreLayerCollision(8, 11, !ignore_Layer); //toggle ignore on kenos layer
+            ignore_Layer = !ignore_Layer; //toggle ignore field
+
+            ToggleWorldVisibility(w_Type);
+        }
+    }
+
+    void ToggleWorldVisibility(bool world_state)
+    {
+        if(world_state)
+        {
+            physical_world.SetActive(true);
+            kenos_world.SetActive(false);
+        }
+        else
+        {
+            physical_world.SetActive(false);
+            kenos_world.SetActive(true);
         }
     }
 }
