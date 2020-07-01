2c0e41491cb8c5a51e46b101452ef5d04c99c100
Rudra Trivedi <trivedirudra2@gmail.com>
2020-07-01 13:55:15 -0400

    BulletTime
    Slow down time instead of stopping it

---
+++ SummerStudio/Assets/Scripts/BulletTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTime : MonoBehaviour
+{
+    public GameObject enemy;
+    public float speed = 1;
+    public float slowdown = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.transform.Translate(Vector3.right * Time.deltaTime * speed * -1);
+        if(Input.GetKey(KeyCode.X))
+        {
+            Time.timeScale = slowdown;
+            Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        }
+    }
+}
