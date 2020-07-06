201a7672b4271215c653249cbecc4132350dec24
SckrPunch <40640248+SckrPunch@users.noreply.github.com>
2020-07-06 19:31:39 -0400

    Collectable system
    Only for machine parts

---
+++ SummerStudio/Assets/Scripts/Collectables.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectables : MonoBehaviour
+{
+    //this script contains interactions for all collectables EXCEPT for health pick ups
+
+    public int machine_parts;
+    public int total_machine_parts;
+    public Image[] mparts;
+
+    private void Start()
+    {
+        machine_parts = 0;
+
+        for (int i = 0; i < total_machine_parts; i++)
+        {
+            mparts[i].enabled = false;
+        }
+    }
+
+    public void PickUp(GameObject obj)
+    {
+        machine_parts++;
+        Destroy(obj);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Machine Parts")
+        {
+            PickUp(collision.gameObject);
+            mparts[machine_parts - 1].enabled = true;
+        }
+    }
+}
