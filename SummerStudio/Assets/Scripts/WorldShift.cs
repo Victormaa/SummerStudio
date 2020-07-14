cfdfa3fdc87babf84c47e13d51c0d48543512fc5
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2020-07-13 20:29:26 -0400

    trying to get world shift transparency to work. toggle for both behaviors

--- SummerStudio/Assets/Scripts/WorldShift.cs
+++ SummerStudio/Assets/Scripts/WorldShift.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.U2D;
+using UnityEngine.U2D;
 
 public class WorldShift : MonoBehaviour
 {
@@ -10,12 +12,20 @@ public class WorldShift : MonoBehaviour
     public GameObject kenos_world;
     public GameObject PostProcessVolume;
     public GameObject gameManager;
+    [SerializeField] private bool hideOtherWorld = true;
+
+    private Color solidColor = new Color(1f, 1f, 1f, 1f);
+    private Color transColor = new Color(1f, 1f, 1f, 0.5f);
+    // SpriteShapeRenderer[] physicalPlatforms;
+    // SpriteShapeRenderer[] kenosPlatforms;
 
     // Start is called before the first frame update
     void Start()
     {
         ToggleWorldVisibility(w_Type);
         Physics2D.IgnoreLayerCollision(8, 11, ignore_Layer);
+        // physicalPlatforms = physical_world.GetComponentsInChildren<SpriteShapeRenderer>();
+        // kenosPlatforms = kenos_world.GetComponentsInChildren<SpriteShapeRenderer>();
     }
 
     // Update is called once per frame
@@ -43,30 +53,47 @@ public class WorldShift : MonoBehaviour
 
     void ToggleWorldVisibility(bool world_state)
     {
-        if(world_state)
-        {
-            physical_world.SetActive(true);
-            kenos_world.SetActive(false);
+        if (hideOtherWorld) {
+            if(world_state)
+            {
+                physical_world.SetActive(true);
+                kenos_world.SetActive(false);
+            }
+            else
+            {
+                physical_world.SetActive(false);
+                kenos_world.SetActive(true);
+            }
         }
-        else
-        {
-            physical_world.SetActive(false);
-            kenos_world.SetActive(true);
+        else {
+            if(world_state)
+            {
+                // foreach(UnityEngine.U2D.SpriteShapeRenderer platform in physicalPlatforms) {
+                //     platform.color = solidColor;
+                // }
+                // foreach(UnityEngine.U2D.SpriteShapeRenderer platform in kenosPlatforms) {
+                //     platform.color = transColor;
+                // }
+                // physical_world.SetActive(true);
+                // kenos_world.SetActive(false);
+            }
+            else
+            {
+                // foreach(UnityEngine.U2D.SpriteShapeRenderer platform in physicalPlatforms) {
+                //     platform.color = transColor;
+                // }
+                // foreach(UnityEngine.U2D.SpriteShapeRenderer platform in kenosPlatforms) {
+                //     platform.color = solidColor;
+                // }
+                // physical_world.SetActive(false);
+                // kenos_world.SetActive(true);
+            }
         }
     }
 
     void ToggleWorldVisibilityWithSlowdown(bool world_state)
     {
-        if(world_state)
-        {
-            physical_world.SetActive(true);
-            kenos_world.SetActive(false);
-        }
-        else
-        {
-            physical_world.SetActive(false);
-            kenos_world.SetActive(true);
-        }
+        ToggleWorldVisibility(world_state);
         gameManager.GetComponent<BulletTime>().EnableBulletTimeWithDuration(0.2f);
     }
 
