cdf5a3367fe3fab3f9f6a08b138b8d9120e5befc
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2020-08-26 12:15:26 -0400

    added fly enemies and power node to attract them

--- SummerStudio/Assets/Scripts/FlyMovement.cs
+++ SummerStudio/Assets/Scripts/FlyMovement.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class FlyMovement : MonoBehaviour
 {
     [SerializeField] private GameObject leader;
-    [SerializeField] private float speed = 2f;
-    
+    [SerializeField] private float speed = 5f;
+    [SerializeField] public GameObject distracter;
+    [SerializeField] private float distractDuration = 5f;
+    private float timeDistracted;
+    private bool isDistracted = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +20,37 @@ public class FlyMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, leader.transform.position, speed * Time.deltaTime);
+        if (isDistracted == false) {
+            transform.position = Vector3.MoveTowards(transform.position, leader.transform.position, speed * Time.deltaTime);
+        }
+        else {
+            if (distracter != null) {
+                transform.position = Vector3.MoveTowards(transform.position, distracter.transform.position, speed * Time.deltaTime);
+            }
+            else {
+                isDistracted = false;
+            }
+            if (Time.time - timeDistracted > distractDuration) {
+                // Debug.Log("distraction end");
+                isDistracted = false;
+            }
+        }
     }
+
+    public void Distract(GameObject distraction) {
+        // Debug.Log("going to distraction");
+        isDistracted = true;
+        timeDistracted = Time.time;
+        distracter = distraction;
+    }
+    
+    public void Distract(GameObject distraction, float duration) {
+        // Debug.Log("going to distraction");
+        isDistracted = true;
+        timeDistracted = Time.time;
+        distracter = distraction;
+        distractDuration = duration;
+    }
+
 
 }
