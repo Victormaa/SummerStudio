38e478dc900ef2af735b7cc1bcf125bfaf91085f
SckrPunch <40640248+SckrPunch@users.noreply.github.com>
2020-08-31 03:38:25 -0400

    Bullet spitter

---
+++ SummerStudio/Assets/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    [SerializeField] private bool bulletDirection; //true = up, false = down
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(bulletDirection)
+        {
+            transform.position += new Vector3(0f, 1f, 0f) * Time.deltaTime * speed;
+        }
+        else
+        {
+            transform.position += new Vector3(0f, -1f, 0f) * Time.deltaTime * speed;
+        }
+    }
+
+    public void SetBulletDirection(bool direction)
+    {
+        bulletDirection = direction;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ SummerStudio/Assets/BulletSpitter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpitter : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject spawnPoint;
+    public bool bulletDirection; //true = up, false = down
+    public bool isActivated;
+    private GameObject bulletObj;
+    [SerializeField] private float currentTime;
+    [SerializeField] private float spawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isActivated)
+        {
+            currentTime += Time.deltaTime;
+
+            if (currentTime >= spawnTime)
+            {
+                Fire();
+                currentTime = 0;
+            }
+        }
+        
+    }
+
+    void Fire()
+    {
+        bulletObj = Instantiate(bullet, spawnPoint.transform.position, Quaternion.identity);
+        bulletObj.name = this.gameObject.name + "bullet";
+        bulletObj.GetComponent<Bullet>().SetBulletDirection(bulletDirection);
+    }
+}
